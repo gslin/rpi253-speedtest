ace3734ae8106ce58b3dd903b91a03281743cfee
Gea-Suan Lin <gslin+bot@gslin.com>
2025-06-10 17:30:50 +0000

    feat: add data into date-month-suffix file.

--- run.sh
+++ run.sh
@@ -4,12 +4,16 @@ set -euo pipefail
 cd "$(dirname $0)"
 
 main() {
+    local D
+    local D
     local R
 
+    D=$(date +%Y-%m)
+    F="rpi253-speedtest-${D}.csv"
     R=$(speedtest-cli --secure --csv)
-    echo "${R}" | tee -a rpi253-speedtest.csv > /dev/null 2>&1
+    echo "${R}" | tee -a "${F}" > /dev/null 2>&1
 
-    git add rpi253-speedtest.csv
+    git add "${F}"
     git commit -m "chore: add by run.sh."
     git push origin main
 }
